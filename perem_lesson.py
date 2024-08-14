8347c2e1542efa0ab7ea30bb71e1e2fb2c2e0107
nadipetrovna <nadipetrovna@gmail.com>
2024-08-14 21:48:22 +0300

    Add files via upload

---
+++ perem_lesson.py
@@ -0,0 +1,15 @@
+# Домашняя работа.Переменные
+spase = ' '
+colon = ': '
+comma = ','
+dot = '.'
+home_work = 12
+Course = 'Python'
+time_all_H_W = 1.5
+string_1 = 'Курс'
+string_2 = 'затрачено часов'
+string_3 = ' среднее время выполнения '
+string_4 = ' часа.'
+one_H_W=time_all_H_W/home_work
+print(string_1+colon+Course+comma+string_2+colon+str(time_all_H_W)+string_3+str(one_H_W)+string_4)
+print(string_1,colon,Course,comma,string_2,colon,str(time_all_H_W),string_3,str(one_H_W),string_4) #лишние пробелы появляются
